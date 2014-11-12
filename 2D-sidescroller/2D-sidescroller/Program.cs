56160a243f99d1743e72618763aad46cf4ea9683
John Rapp <johnfarnes@hotmail.com>
2014-11-12 17:41:21 +0100

    Fixed the merge issue

--- 2D-sidescroller/2D-sidescroller/Program.cs
+++ 2D-sidescroller/2D-sidescroller/Program.cs
@@ -3,14 +3,11 @@ using OpenTK;
 using OpenTK.Graphics;
 using OpenTK.Graphics.OpenGL;
 using OpenTK.Input;
-<<<<<<< HEAD
 using System.Diagnostics;
-=======
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
 using System.Reflection;
->>>>>>> origin/master
 
 namespace _2D_sidescroller
 {
@@ -18,19 +15,17 @@ namespace _2D_sidescroller
     {
 		class Game : GameWindow
 		{
-<<<<<<< HEAD
-			private uint[] Buffers = new uint[2];
+			private uint[] Buffers = new uint[3];
 
 			private int BUFFER_POSITIONS = 0;
 			private int BUFFER_TEXCOORDS = 1;
+			private int BUFFER_INDICES = 2;
 
 			int VertexShader, FragmentShader, ShaderProgram;
 
 			int PositionAttribute, TexCoordAttribute;
-=======
 
 			uint PlayerTexture;
->>>>>>> origin/master
 
 			public Game()
 				: base(800, 600, GraphicsMode.Default, "John och Simons superspel")
@@ -45,7 +40,7 @@ namespace _2D_sidescroller
 
 				GL.ClearColor(0.1f, 0.2f, 0.5f, 0.0f);
 				GL.Enable(EnableCap.DepthTest);
-<<<<<<< HEAD
+				GL.Enable(EnableCap.Texture2D);
 				GL.DepthFunc(DepthFunction.Lequal);
 
 				InitShaders();
@@ -53,6 +48,8 @@ namespace _2D_sidescroller
 				InitAttributes();
 
 				InitBuffers();
+
+				PlayerTexture = (uint)LoadTexture(getFilepathInDebugDir("player.png"));
 			}
 
 			private void InitShaders()
@@ -66,7 +63,7 @@ namespace _2D_sidescroller
 					varying lowp float vTexCoord;
 
 					void main() {
-						gl_Position = vec4(aPosition);
+						gl_Position = vec4(aPosition, 1.0);
 						vTexCoord = aTexCoord;
 					}
 
@@ -82,15 +79,15 @@ namespace _2D_sidescroller
 					}
 				";
 
-				VertexShader = GL.CreateShader(ShaderType.FragmentShader);
+				VertexShader = GL.CreateShader(ShaderType.VertexShader);
 				GL.ShaderSource(VertexShader, vertexShaderSrc);
 				GL.CompileShader(VertexShader);
-				Debug.WriteLine(GL.GetShaderInfoLog(VertexShader));
+				//Debug.WriteLine(GL.GetShaderInfoLog(VertexShader));
 
 				FragmentShader = GL.CreateShader(ShaderType.FragmentShader);
 				GL.ShaderSource(FragmentShader, fragmentShaderSrc);
 				GL.CompileShader(FragmentShader);
-				Debug.WriteLine(GL.GetShaderInfoLog(FragmentShader));
+				//Debug.WriteLine(GL.GetShaderInfoLog(FragmentShader));
 
 				// Create program
 				ShaderProgram = GL.CreateProgram();
@@ -110,7 +107,7 @@ namespace _2D_sidescroller
 
 			private void InitBuffers()
 			{
-				GL.GenBuffers(2, Buffers);
+				GL.GenBuffers(3, Buffers);
 
 				GL.BindBuffer(BufferTarget.ArrayBuffer, Buffers[BUFFER_POSITIONS]);
 
@@ -125,28 +122,34 @@ namespace _2D_sidescroller
 				//Amount of data per vertex
 				int positionAmount = 2;
 
-				GL.BufferData(BufferTarget.ArrayBuffer, (IntPtr)(positions.Length * positionAmount * sizeof(float)), positions, BufferUsageHint.StaticDraw);
+				GL.BufferData(BufferTarget.ArrayBuffer, (IntPtr)(positions.Length * sizeof(float)), positions, BufferUsageHint.StaticDraw);
 
 
 				GL.BindBuffer(BufferTarget.ArrayBuffer, Buffers[BUFFER_POSITIONS]);
 
 				float[] texCoords = new float[] {
-				//  x	  y
-					0.0f, 1.0f,
-					1.0f, 1.0f,
-					1.0f, 0.0f,
-					0.0f, 0.0f
+				//   x	  y
+					-1, -1, 0,
+					-1,  1, 0,
+					 1,  1, 0,
+					 1, -1, 0
 
 				};
 				//Amount of data per vertex
 				int texcoordAmount = 2;
 
-				GL.BufferData(BufferTarget.ArrayBuffer, (IntPtr)(texCoords.Length * texcoordAmount * sizeof(float)), texCoords, BufferUsageHint.StaticDraw);
-=======
-				GL.Enable(EnableCap.Texture2D);
+				GL.BufferData(BufferTarget.ArrayBuffer, (IntPtr)(texCoords.Length * sizeof(float)), texCoords, BufferUsageHint.StaticDraw);
 
-				PlayerTexture = (uint)LoadTexture(getFilepathInDebugDir("player.png"));
->>>>>>> origin/master
+				GL.BindBuffer(BufferTarget.ElementArrayBuffer, Buffers[BUFFER_INDICES]);
+
+				float[] indices = new float[] {
+					0, 1, 2,
+                    0, 2, 3
+				};
+				//Amount of data per vertex
+				int indicesAmount = 1;
+
+				GL.BufferData(BufferTarget.ElementArrayBuffer, (IntPtr)(indices.Length * sizeof(float)), indices, BufferUsageHint.StaticDraw);
 			}
 
 			
@@ -188,6 +191,13 @@ namespace _2D_sidescroller
 				}
 			}
 
+			protected override void OnKeyDown(KeyboardKeyEventArgs e)
+			{
+				base.OnKeyDown(e);
+			
+				//TODO use this instead
+			}
+
 			protected override void OnRenderFrame(FrameEventArgs e)
 			{
 				base.OnRenderFrame(e);
@@ -204,8 +214,12 @@ namespace _2D_sidescroller
 
 				GL.BindBuffer(BufferTarget.ArrayBuffer, Buffers[BUFFER_TEXCOORDS]);
 				GL.VertexAttribPointer(PositionAttribute, 2, VertexAttribPointerType.Float, false, 0, 0);
+
+				GL.BindBuffer(BufferTarget.ElementArrayBuffer, Buffers[BUFFER_INDICES]);
 				
-				GL.DrawArrays(PrimitiveType.Triangles, 0, 4);
+				GL.DrawArrays(PrimitiveType.Triangles, 0, 6);
+
+				SwapBuffers();
 			}
 
 			protected override void OnClosed(EventArgs e)
